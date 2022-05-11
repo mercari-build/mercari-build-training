1e86e8ee91a8c062179c9ecfb6dda7eec33cb038
littlibanana <66011133+littlibanana@users.noreply.github.com>
2022-05-11 15:42:10 +0800

    finished 3-2 and 3-3

--- python/main.py
+++ python/main.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import pathlib
+import json
 from fastapi import FastAPI, Form, HTTPException
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
@@ -9,31 +10,56 @@ app = FastAPI()
 logger = logging.getLogger("uvicorn")
 logger.level = logging.INFO
 images = pathlib.Path(__file__).parent.resolve() / "image"
-origins = [ os.environ.get('FRONT_URL', 'http://localhost:3000') ]
+origins = [os.environ.get('FRONT_URL', 'http://localhost:3000')]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=False,
-    allow_methods=["GET","POST","PUT","DELETE"],
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
 )
+
 
 @app.get("/")
 def root():
     return {"message": "Hello, world!"}
 
-@app.post("/items")
-def add_item(name: str = Form(...)):
-    logger.info(f"Receive item: {name}")
-    return {"message": f"item received: {name}"}
 
-@app.get("/image/{items_image}")
+@app.get("/items")
+def get_item():
+    f = open('items.json')
+    file = json.load(f)
+    return file
+
+
+@app.post("/items")
+def add_item(name: str = Form(...), category: str = Form(...)):
+    logger.info(f"Receive item: {name, category}")
+
+    new_item = {"name": name,
+                "category": category}
+
+    with open('items.json', 'r+') as file:
+        # First we load existing data into a dict.
+        file_data = json.load(file)
+        # Join new_data with file_data inside emp_details
+        file_data["items"].append(new_item)
+        # Sets file's current position at offset.
+        file.seek(0)
+        # convert back to json.
+        json.dump(file_data, file, indent=4)
+
+    return {"message": f"item received: {name, category}"}
+
+
+@ app.get("/image/{items_image}")
 async def get_image(items_image):
     # Create image path
     image = images / items_image
 
     if not items_image.endswith(".jpg"):
-        raise HTTPException(status_code=400, detail="Image path does not end with .jpg")
+        raise HTTPException(
+            status_code=400, detail="Image path does not end with .jpg")
 
     if not image.exists():
         logger.debug(f"Image not found: {image}")
