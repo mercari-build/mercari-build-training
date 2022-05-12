57e163f8e23b3377883f43cc057ac6a63b2be2b2
NyatsuYAI <natsu.72bad@gmail.com>
2022-05-13 00:43:10 +0900

    post json tried but  it return 422

--- python/main.py
+++ python/main.py
@@ -1,9 +1,15 @@
+from asyncore import file_dispatcher
+from calendar import c
 import os
 import logging
 import pathlib
 from fastapi import FastAPI, Form, HTTPException
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
+
+import json
+from pathlib import Path
+
 
 app = FastAPI()
 logger = logging.getLogger("uvicorn")
@@ -18,14 +24,31 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
+def add_json(data,file_json='item.json'):
+    path = Path(file_json)
+    if not path.is_file():
+        file_data={"items": []}
+    else:
+        with open(file_json,'r')as f:
+            file_data=json.load(f)
+
+
+    with open(file_json, 'w') as f:
+        file_data["items"].append(data)
+        json.dump(file_data, f)
+
+
 @app.get("/")
 def root():
     return {"message": "Hello, world!"}
 
+
 @app.post("/items")
-def add_item(name: str = Form(...)):
-    logger.info(f"Receive item: {name}")
-    return {"message": f"item received: {name}"}
+def add_item(name: str = Form(...), category: str = Form(...)):
+    add_json({"name": name, "category": category})
+    logger.info(f"Receive item: {name} , {category}")
+    return {"message": f"item received: {name} , {category}"}
 
 @app.get("/image/{items_image}")
 async def get_image(items_image):
