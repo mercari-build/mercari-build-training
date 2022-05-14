288b9ac1fdf96bb8434c5e153690bbd92beac5ba
nsmryk <yuki.nishimura369@gmail.com>
2022-05-14 14:08:40 +0900

    Modify conflicts

--- go/app/main.go
+++ go/app/main.go
@@ -25,14 +25,54 @@ func root(c echo.Context) error {
 	return c.JSON(http.StatusOK, res)
 }
 
+func handleError(c echo.Context, error_message string) error {
+	c.Logger().Errorf("%s", error_message)
+	res := Response{Message: error_message} 
+ 	return c.JSON(http.StatusBadRequest, res) 
+}
+
 func addItem(c echo.Context) error {
 	// Get form data
 	name := c.FormValue("name")
-	c.Logger().Infof("Receive item: %s", name)
+	category := c.FormValue("category")
+	item := Item{name, category}
+	c.Logger().Infof("Receive item: %s %s", name, category)
 
-	message := fmt.Sprintf("item received: %s", name)
+	// Read items.json
+	fp, err := os.OpenFile("items.json", os.O_RDWR|os.O_CREATE, 0664)
+	if err != nil {
+		handleError(c, "Failed to open items.json")
+	}
+	defer fp.Close()
+
+	file, err := ioutil.ReadAll(fp)
+	if err != nil {
+		handleError(c, "Failed to read the file")
+	}
+
+	// Add item
+	var items Items
+	if len(file) != 0 {
+		err = json.Unmarshal(file, &items)
+		if err != nil {
+			handleError(c, "Failed to encode items to a JSON string")
+		}
+		items.Items = append(items.Items, item)
+	} else {
+		items.Items = [] Item{item}
+	}
+	output, err := json.MarshalIndent(&items, "", "\t")
+	if err != nil {
+		handleError(c, "Failed to encode items to a JSON string")
+	}
+
+	// Write json
+	err = ioutil.WriteFile("items.json", output, 0644)
+	if err != nil {
+		handleError(c, "Failed to save data in json file")
+	}
+	message := fmt.Sprintf("item received: %s %s", name, category)
 	res := Response{Message: message}
-
 	return c.JSON(http.StatusOK, res)
 }
 
