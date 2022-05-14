911aff00ce74bc07929437142f7e13e3e753eae6
nnnngo <44683382+nnnngo@users.noreply.github.com>
2022-05-14 17:37:39 +0900

    setup SQLite3 as a DB

---
+++ go/app/config/config.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+    "log"
+    "os"
+
+    "gopkg.in/ini.v1"
+)
+
+type ConfigList struct {
+    DbName        string
+    SQLDriver     string
+}
+
+var Config ConfigList
+
+func init() {
+    cfg, err := ini.Load("./config/config.ini")
+    if err != nil {
+        log.Printf("Failed to read file: %v", err)
+        os.Exit(1)
+    }
+
+    Config = ConfigList{
+        DbName:    cfg.Section("db").Key("name").String(),
+        SQLDriver: cfg.Section("db").Key("driver").String(),
+    }
+}
--- go/app/main.go
+++ go/app/main.go
@@ -10,11 +10,22 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/gommon/log"
+
+	"mercari-build-training-2022/app/models"
 )
 
 const (
 	ImgDir = "image"
 )
+
+type Item struct {
+	Name string `json:"name"`
+	Category string `json:"category"`
+}
+
+type Items struct {
+	Items []Item `json:"items"`
+}
 
 type Response struct {
 	Message string `json:"message"`
@@ -25,13 +36,110 @@ func root(c echo.Context) error {
 	return c.JSON(http.StatusOK, res)
 }
 
-func addItem(c echo.Context) error {
-	// Get form data
-	name := c.FormValue("name")
-	category := c.FormValue("category")
-	c.Logger().Infof("Receive item: %s which belongs to the category %s", name, category)
+func getItems(c echo.Context) error {
+	var items Items
+	// path := "./items.json"
 
-	message := fmt.Sprintf("item received: %s which belongs to the category %s", name, category)
+	// Open JSON file and read raw binanry
+	// raw, err := os.ReadFile(path)
+	// if _, err := os.Stat(path); os.IsNotExist(err) {
+	// 	res := Response{Message: "No items"}
+	// 	return c.JSON(http.StatusOK, res)
+	// }
+
+	// Covert raw binary to the Items struct
+	// err = json.Unmarshal([]byte(raw), &items)
+	// if err != nil {
+	// 	return c.JSON(http.StatusInternalServerError, err)
+	// }
+
+	// Init DB
+	db := db.DbConnection
+	defer db.Close()
+
+	// Exec Query
+	rows, err := db.Query(`SELECT name, category FROM items`)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var name string
+		var category string
+
+		// カーソルから値を取得
+		if err := rows.Scan(&name, &category); err != nil {
+			return c.JSON(http.StatusInternalServerError, err)
+		}
+
+		fmt.Printf("name: %d, category: %s\n", name, category)
+		items.Items = append(items.Items, Item{Name: name, Category: category})
+	}
+
+	return c.JSON(http.StatusOK, items)
+}
+
+func addItem(c echo.Context) error {
+	// IInintialize Items
+	// var items Items
+	// Inintialize Item
+	var item Item
+	// Get form data
+	item.Name = c.FormValue("name")
+	item.Category = c.FormValue("category")
+
+	c.Logger().Infof("Receive item: %s which belongs to the category %s", item.Name, item.Category)
+
+	message := fmt.Sprintf("item received: %s which belongs to the category %s", item.Name, item.Category)
+
+	// Read raw data of items from items.json
+	// path := "./items.json"
+	// if _, err := os.Stat(path); os.IsNotExist(err) {
+	// 	_, err := os.Create(path)
+	// 	if err != nil {
+	// 		return c.JSON(http.StatusInternalServerError, err)
+	// 	}
+	// }
+	// raw, err := os.ReadFile(path)
+	// if err != nil {
+	// 	return c.JSON(http.StatusInternalServerError, err)
+	// }
+	
+	// if len(raw) != 0 {
+	// 	// Convert raw data to the Items struct
+	// 	err = json.Unmarshal([]byte(raw), &items)
+	// 	if err != nil {
+	// 		return c.JSON(http.StatusInternalServerError, err)
+	// 	}
+	// }
+
+	// Add Item to items
+	// items.Items = append(items.Items, item)
+
+	// Convert Items to raw JSON data
+	// raw, err = json.MarshalIndent(items, "", " ")
+	// if err != nil {
+	// 	return c.JSON(http.StatusInternalServerError, err)
+	// }
+
+	// Write Items to items.json
+	// err = os.WriteFile("./items.json", raw, 0644)
+	// if err != nil {
+	// 	return c.JSON(http.StatusInternalServerError, err)
+	// }
+
+	// Init DB
+	db := db.DbConnection
+	c.Logger().Infof("DB Initialized")
+	defer db.Close()
+
+	// Exec Query
+	_, err := db.Exec(`INSERT INTO items (name, category) VALUES (?, ?)`, item.Name, item.Category)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+	
 	res := Response{Message: message}
 
 	return c.JSON(http.StatusOK, res)
@@ -71,6 +179,7 @@ func main() {
 
 	// Routes
 	e.GET("/", root)
+	e.GET("/items", getItems)
 	e.POST("/items", addItem)
 	e.GET("/image/:itemImg", getImg)
 
