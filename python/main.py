dab319b822797d20a740741b17f94f665e7c3591
Ayumi Uki <chebrinko@gmail.com>
2022-05-14 16:09:12 +0900

    update main.py for 3-2 and 3-3

--- python/main.py
+++ python/main.py
@@ -5,6 +5,8 @@ from fastapi import FastAPI, Form, HTTPException
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 
+import json
+
 app = FastAPI()
 logger = logging.getLogger("uvicorn")
 logger.level = logging.INFO
@@ -18,14 +20,49 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+file_json = 'items.json'
+if not os.path.isfile(file_json):
+    initial_items_data = {"items": []}
+    with open(file_json, 'w') as outfile:
+        json.dump(initial_items_data, outfile)
+
+def add_item_to_json(item, file_json):
+    try:
+        with open(file_json, 'r') as f:
+            items_data = json.load(f)
+    except json.JSONDecodeError as e:
+        print('JSONDecodeError', e)
+    
+    items_data["items"].append(item)
+
+    with open('items.json', 'w') as outfile:
+        json.dump(items_data, outfile)
+
+    print(f'added {item} to {file_json}')
+
+
 @app.get("/")
 def root():
     return {"message": "Hello, world!"}
 
 @app.post("/items")
-def add_item(name: str = Form(...)):
-    logger.info(f"Receive item: {name}")
-    return {"message": f"item received: {name}"}
+def add_item(name: str = Form(...), category: str = Form(...)):
+    logger.info(f"Receive item: {name}, {category}")
+    item = {"name": name, "category": category}
+    add_item_to_json(item, file_json)
+    return {"name": name, "category": category}
+
+@app.get("/items")
+def get_items():
+    if not os.path.isfile(file_json):
+        print('')
+    try:
+        with open('items.json', 'r') as f:
+            data = json.load(f)
+    except json.JSONDecodeError as e:
+        print('JSONDecodeError', e)
+    print(data)
+    return data
 
 @app.get("/image/{items_image}")
 async def get_image(items_image):
