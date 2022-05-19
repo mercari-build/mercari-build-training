582bd44637ad2ac766d34e13dff493e6d8e55466
Satyabrata Pahari <p-satyabrata-build2022@mercari-partner.com>
2022-05-19 15:55:41 +0900

    3

--- python/main.py
+++ python/main.py
@@ -1,9 +1,16 @@
 import os
+import json
+import sqlite3
+import hashlib
 import logging
 import pathlib
+import sys
+#from numpy import integer
+import requests
 from fastapi import FastAPI, Form, HTTPException
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 logger = logging.getLogger("uvicorn")
@@ -18,25 +25,118 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+#connect to the database
+mycon = sqlite3.connect('mercari.sqlite3',check_same_thread=False)
+c = mycon.cursor()
+# c.execute("DROP TABLE items")
+# c.execute("CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY, name STRING, category STRING, image STRING);")
+mycon.commit()
+#close the connection
+mycon.close()
+
+#function to has the image in path spcifies
+def hash_image(image):
+    #error message if image path doesnt end with .jpg
+    if not image.endswith(".jpg"):
+        raise HTTPException(status_code=400, detail="Image path does not end with .jpg")
+
+    #open the file as read binary and hash 
+    with open(image,"rb") as f:
+        bytes = f.read() # read entire file as bytes
+        readable_hash = hashlib.sha256(bytes).hexdigest();
+        f.close()
+
+    #return the hashed image
+    return (f'{readable_hash}.jpg')
+        
+
 @app.get("/")
 def root():
     return {"message": "Hello, world!"}
-
+    
+data =[];
+data1 ={"items": data}
 @app.post("/items")
-def add_item(name: str = Form(...)):
-    logger.info(f"Receive item: {name}")
-    return {"message": f"item received: {name}"}
+def add_item(name: str = Form(...),category: str = Form(...), image: str = Form(...)):
+    mycon = sqlite3.connect('mercari.sqlite3',check_same_thread=False)
+    c = mycon.cursor()
+    hashed_image = hash_image(image)
+    c.execute("INSERT INTO items (name,category,image) VALUES (?,?,?)",(name,category,hashed_image))
+    mycon.commit()
+    mycon.close()
+    #  data.append({"name": name, "category": category})
+    #  with open('items.json', "w") as file:
+    #      json.dump(data1, file)
+    
+    logger.info(f"Receive item: {name}, category: {category}, image: {hashed_image}" )
+    return {"message": f"item received: {name}, category: {category},, image: {hashed_image}"}
+
+# @app.get("/search?keyword={search_word}")
+# def get_search(search_word):
+#     
+     
+# @app.get("/items")
+# def get_item():
+#     return c.execute("SELECT * FROM items");
+
+@app.get("/items")
+def get_item():
+    mycon = sqlite3.connect('mercari.sqlite3',check_same_thread=False)
+    mycon.row_factory = sqlite3.Row
+    c = mycon.cursor()
+    rows = c.execute("SELECT * FROM items").fetchall()
+    mycon.commit()
+    mycon.close()
+
+    data = []    
+    for row in rows:
+        columns = row.keys()
+        data.append({key: row[key] for key in columns})
+
+    return {"items": data}
+    # with open('items.json', "r") as file:
+    #     data1 = json.load(file).``
+    # return data1
+
+
+
+@app.get("/search")
+def search_items(keyword: str):
+    mycon = sqlite3.connect('mercari.sqlite3',check_same_thread=False)
+    mycon.row_factory = sqlite3.Row
+    c = mycon.cursor()
+    rows = c.execute("SELECT * FROM Items WHERE name LIKE ? OR category LIKE ?",(keyword,keyword)).fetchall()
+    mycon.commit()
+    mycon.close()
+
+    data = []    
+    for row in rows:
+        columns = row.keys()
+        data.append({key: row[key] for key in columns})
+    return {"items": data}
+
+
+@app.get("/items/{item_id}")
+def get_item_id(item_id):
+    mycon = sqlite3.connect('mercari.sqlite3',check_same_thread=False)
+    mycon.row_factory = sqlite3.Row
+    c = mycon.cursor()
+    rows = c.execute("SELECT name,category,image FROM items WHERE id LIKE ?",(item_id,)).fetchall()
+    mycon.commit()
+    mycon.close()
+
+    data = []    
+    for row in rows:
+        columns = row.keys()
+        data.append({key: row[key] for key in columns})
+
+    return data
+
+
 
 @app.get("/image/{items_image}")
 async def get_image(items_image):
     # Create image path
     image = images / items_image
 
-    if not items_image.endswith(".jpg"):
-        raise HTTPException(status_code=400, detail="Image path does not end with .jpg")
 
-    if not image.exists():
-        logger.debug(f"Image not found: {image}")
-        image = images / "default.jpg"
-
-    return FileResponse(image)
