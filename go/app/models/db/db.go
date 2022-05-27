8a8924e48ec0fe5028408cfec90b34ea1f4e9bed
Iinuma Mizuki <imiinz1331@gmail.com>
2022-05-27 23:53:56 +0900

    create sql to create tables related to qas

--- go/app/models/db/db.go
+++ go/app/models/db/db.go
@@ -1,12 +1,12 @@
 package db
 
 import (
-    "os"
-    "database/sql"
-    "fmt"
-    "log"
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
 
-    _ "github.com/mattn/go-sqlite3"
+	_ "github.com/mattn/go-sqlite3"
 
 	"mercari-build-training-2022/app/config"
 )
@@ -14,18 +14,18 @@ import (
 var DbConnection *sql.DB
 
 func init() {
-    var err error
-    if env := os.Getenv("ENV"); env == "test" {
-        DbConnection, err = sql.Open(config.Config.SQLDriver, config.Config.TestDbName)
-    } else {
-        DbConnection, err = sql.Open(config.Config.SQLDriver, config.Config.DbName)
-    }
-    if err != nil {
-        log.Fatalln(err)
-    }
-    log.Printf(os.Getenv("Exec db cmd"))
-    // CREATE DB TABLES
-    cmd := fmt.Sprintf(`
+	var err error
+	if env := os.Getenv("ENV"); env == "test" {
+		DbConnection, err = sql.Open(config.Config.SQLDriver, config.Config.TestDbName)
+	} else {
+		DbConnection, err = sql.Open(config.Config.SQLDriver, config.Config.DbName)
+	}
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Printf(os.Getenv("Exec db cmd"))
+	// CREATE DB TABLES
+	cmd := fmt.Sprintf(`
         CREATE TABLE IF NOT EXISTS [users] (
             id INTEGER PRIMARY KEY AUTOINCREMENT, 
             name STRING UNIQUE NOT NULL,
@@ -55,9 +55,23 @@ func init() {
             foreign key (buyer_id) REFERENCES users(id) ON DELETE CASCADE,
             foreign key (transaction_status_id) REFERENCES transaction_statuses(id) ON DELETE CASCADE,
             UNIQUE (item_id, buyer_id)
+        );
+        CREATE TABLE IF NOT EXISTS qa_types(
+            id INTEGER PRIMARY KEY AUTOINCREMENT, 
+            label STRING
+        );
+        CREATE TABLE IF NOT EXISTS qas(
+            id INTEGER PRIMARY KEY AUTOINCREMENT, 
+            item_id INTEGER NOT NULL,
+            question STRING,
+            answer STRING,
+            qa_type_id INTEGER NOT NULL,
+            foreign key (item_id) REFERENCES items(id) ON DELETE CASCADE,
+            foreign key (qa_type_id) REFERENCES qa_types(id) ON DELETE CASCADE,
+            UNIQUE (item_id)
         )
         `)
-    _, err = DbConnection.Exec(cmd)
+	_, err = DbConnection.Exec(cmd)
 	if err != nil {
 		log.Fatalln(err)
 	}
