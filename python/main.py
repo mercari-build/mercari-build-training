945ef5b4b91f466e915326a7490f3cadb8364035
Liu Tianpin <tianpinliu0208@gmail.com>
2023-05-11 00:15:21 +0900

    task3

--- python/main.py
+++ python/main.py
@@ -1,15 +1,18 @@
+import json
 import os
 import logging
 import pathlib
-from fastapi import FastAPI, Form, HTTPException
+import hashlib
+from fastapi import FastAPI, Form, HTTPException, File, UploadFile
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 logger = logging.getLogger("uvicorn")
 logger.level = logging.INFO
 images = pathlib.Path(__file__).parent.resolve() / "images"
-origins = [ os.environ.get('FRONT_URL', 'http://localhost:3000') ]
+origins = [os.environ.get('FRONT_URL', 'http://localhost:3000')]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -22,10 +25,39 @@ app.add_middleware(
 def root():
     return {"message": "Hello, world!"}
 
+@app.get("/items")
+def get_item():
+    try:
+        with open("items.json", "r") as f:
+            mydata = json.load(f)
+            return mydata
+    except FileNotFoundError:
+        return {}
+
 @app.post("/items")
-def add_item(name: str = Form(...)):
-    logger.info(f"Receive item: {name}")
-    return {"message": f"item received: {name}"}
+def add_item(name: str = Form(...), category: str = Form(...), image: UploadFile = File(...)):
+    logger.info(f"Receive item: {name}, Receive category: {category}, Receive image:{image.filename}")
+
+    # Hash the image using sha256, and save it with the name <hash>.jpg
+    file = image.file.read()
+    image_hash = hashlib.sha256(file).hexdigest()
+    filename = image_hash + ".jpg"
+    path = "images/" + filename
+    with open(path, "w") as f:
+        f.write(filename)
+
+    # Add new items into json file
+    try:
+        with open("items.json", "r") as f:
+            items = json.load(f)
+    except FileNotFoundError:
+        items = {"items": []}
+
+    items["items"].append({"name": name, "category": category, "image_filename": filename})
+    with open("items.json", "w") as f:
+        json.dump(items, f)
+
+    return {"message": f"item received: {name}, category received: {category}, image received: {filename}"}
 
 @app.get("/image/{image_filename}")
 async def get_image(image_filename):
@@ -40,3 +72,12 @@ async def get_image(image_filename):
         image = images / "default.jpg"
 
     return FileResponse(image)
+
+@app.get("/items/{item_id}")
+def get_itemsid(item_id:int):
+    try:
+        with open("items.json", "r") as f:
+            mydata = json.load(f)
+            return mydata["items"][item_id]
+    except:
+        logger.debug("item is not accessible")
