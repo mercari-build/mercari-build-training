6d989418ce565281248352d41b2968504ea25866
dongurikoko <oomotoshiori@oomotoshiorinoMacBook-Pro.local>
2024-02-18 04:54:19 +0900

    addItem,getAllItems実装

--- go/app/main.go
+++ go/app/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 	"path"
@@ -25,15 +27,74 @@ func root(c echo.Context) error {
 	return c.JSON(http.StatusOK, res)
 }
 
+type Item struct {
+	Name     string `json:"name"`
+	Category string `json:"category"`
+}
+
+type AllItems struct {
+	Items []Item `json:"items"`
+}
+
 func addItem(c echo.Context) error {
-	// Get form data
+	// リクエストボディからデータを取得
 	name := c.FormValue("name")
+	category := c.FormValue("category")
+
+	// 既存のデータを読み込む
+	allItems, err := LoadItems()
+	if err != nil {
+		res := Response{Message: "Failed to load items.json"}
+		return c.JSON(http.StatusInternalServerError, res)
+	}
+
+	// 新しいデータを追加
+	allItems.Items = append(allItems.Items, Item{Name: name, Category: category})
+
+	// ファイルに書き込み
+	jsonData, err := json.MarshalIndent(allItems, "", "  ")
+	if err != nil {
+		res := Response{Message: "Failed to marshal allItems"}
+		return c.JSON(http.StatusInternalServerError, res)
+	}
+	err = os.WriteFile("items.json", jsonData, 0644)
+	if err != nil {
+		res := Response{Message: "Failed to write items.json"}
+		return c.JSON(http.StatusInternalServerError, res)
+	}
+
 	c.Logger().Infof("Receive item: %s", name)
 
-	message := fmt.Sprintf("item received: %s", name)
+	message := fmt.Sprintf("item received: name=%s,category=%s", name, category)
 	res := Response{Message: message}
 
 	return c.JSON(http.StatusOK, res)
+}
+
+func getAllItems(c echo.Context) error {
+	allItems, err := LoadItems()
+	if err != nil {
+		res := Response{Message: "Failed to load items.json"}
+		return c.JSON(http.StatusInternalServerError, res)
+	}
+
+	return c.JSON(http.StatusOK, allItems)
+}
+
+func LoadItems() (*AllItems, error) {
+	jsonFile, err := os.Open("items.json")
+	if err != nil {
+		return nil, err
+	}
+	defer jsonFile.Close()
+	jsonData, err := io.ReadAll(jsonFile)
+	if err != nil {
+		return nil, err
+	}
+	var allItems AllItems
+	json.Unmarshal(jsonData, &allItems)
+
+	return &allItems, nil
 }
 
 func getImg(c echo.Context) error {
@@ -71,9 +132,8 @@ func main() {
 	// Routes
 	e.GET("/", root)
 	e.POST("/items", addItem)
+	e.GET("/items", getAllItems)
 	e.GET("/image/:imageFilename", getImg)
-
-
 	// Start server
 	e.Logger.Fatal(e.Start(":9000"))
 }
