44a29e6d511f0e50300340352d9d3db58cb9a03b
YOUNGJUNE <cerealmaster@naver.com>
2024-02-18 22:57:01 +0100

    3-3,3-4,3-5 done

--- go/app/main.go
+++ go/app/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
 	"path"
+	"strconv"
 	"strings"
 
 	"github.com/labstack/echo/v4"
@@ -21,24 +25,15 @@ type Response struct {
 	Message string `json:"message"`
 }
 
-/*
-3-3 Get a list of items
+type Items struct {
+	Items []Item `json:"items"`
+}
 
-	<---- no need struct since error type return
-	only requires entire json to string. if something
-	has to done with seperate items or item, use this code
-	---->
-	items : [{name,category},,,]
-	declare two structs for items and item
-	type Items struct {
-		Items []Item `json:"items"`
-	}
-
-	type Item struct {
-		Name     string `json:"name"`
-		Category string `json:"category"`
-	}
-*/
+type Item struct {
+	Name      string `json:"name"`
+	Category  string `json:"category"`
+	ImageName string `json:"image_name"`
+}
 
 func root(c echo.Context) error {
 	res := Response{Message: "Hello, world!"}
@@ -46,42 +41,122 @@ func root(c echo.Context) error {
 }
 
 /*
-3-3 Get a list of items
+3-4 POST Add an image to an item
 
-	GET getItem (<echo.Context> C)
-	parse the json file from items.json
-	return entire elements of items.json
-	as array.
+	returns Sha256 hashed string of File
 */
-func getItem(c echo.Context) error {
-	json, err := os.Open("items.json")
-
+func getFileSha256(c echo.Context, fileType string) (string, error) {
+	fileHeader, err := c.FormFile(fileType)
 	if err != nil {
-		res := "items.json file not exist!"
-		return c.JSON(http.StatusOK, res)
+		return "", err
 	}
-	defer json.Close()
-
-	//ioutil.ReadAll deprecated from go 1.16
-	byteValue, err := io.ReadAll(json)
+	f, err := fileHeader.Open()
 	if err != nil {
-		res := "no items exist!"
-		return c.JSON(http.StatusOK, res)
+		return "", err
+	}
+	defer f.Close()
+
+	h := sha256.New()
+	if _, err := io.Copy(h, f); err != nil {
+		return "", err
 	}
 
-	ret := string(byteValue)
-	return c.JSONBlob(http.StatusOK, []byte(ret))
+	return hex.EncodeToString(h.Sum(nil)), nil
 }
 
+/*
+open given files and parse json items,
+return items.Items
+*/
+func readItemsFromFile(filePath string) ([]Item, error) {
+	jsonFile, err := os.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+	var items Items
+	if err := json.Unmarshal(jsonFile, &items); err != nil {
+		return nil, err
+	}
+	return items.Items, nil
+}
+
+/*
+3-3 GET get a list of items
+*/
+func getItem(c echo.Context) error {
+	items, err := readItemsFromFile("items.json")
+
+	if err != nil {
+		msg := "items.json file not exist!"
+		return c.JSON(http.StatusBadRequest, msg)
+	}
+	return c.JSON(http.StatusOK, items)
+}
+
+/*
+3-2, 3-4 POST add the list of item
+
+ 1. Get form data, make new <Item> newItem variable
+ 2. store current items in []item array.
+ 3. append newItem in items array, marshal item to valid Json
+ 4. write items.json with updated items
+*/
 func addItem(c echo.Context) error {
 	// Get form data
 	name := c.FormValue("name")
+	category := c.FormValue("category")
+	//get hashed image
+	image, err := getFileSha256(c, "image")
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+	image += ".jpg"
 	c.Logger().Infof("Receive item: %s", name)
-
+	newItem := Item{
+		Name:      name,
+		Category:  category,
+		ImageName: image,
+	}
 	message := fmt.Sprintf("item received: %s", name)
+	// Open items file
+	items, err := readItemsFromFile("items.json")
+	// append newItem in items
+	if err != nil {
+		msg := "error happend while reading elements in items.json!"
+		return c.JSON(http.StatusBadRequest, msg)
+	}
+	items = append(items, newItem)
+	// marshal new items, write back to items.json
+	updatedItems, err := json.Marshal(&items)
+	if err != nil {
+		msg := "error happend while converting new items to json structure!"
+		return c.JSON(http.StatusBadRequest, msg)
+	}
+	os.WriteFile("items.json", updatedItems, 0644)
+
 	res := Response{Message: message}
 
 	return c.JSON(http.StatusOK, res)
+}
+
+/*
+3-5 GET Return item details
+
+ 1. Unmarshal current items in []items array
+ 2. return items[itemId]
+*/
+func getItemById(c echo.Context) error {
+	itemId := c.Param("itemId")
+	items, err := readItemsFromFile("items.json")
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+	parsedInteger, err := strconv.ParseInt(itemId, 10, 64)
+	if err != nil || parsedInteger >= int64(len(items)) {
+		msg := "Error occured while converting itemId to integer!"
+		return c.JSON(http.StatusBadRequest, msg)
+	}
+	return c.JSON(http.StatusOK, items[parsedInteger])
 }
 
 func getImg(c echo.Context) error {
@@ -119,8 +194,8 @@ func main() {
 	// Routes
 	e.GET("/", root)
 	e.POST("/items", addItem)
-	//3-3. GET a list of items
 	e.GET("/items", getItem)
+	e.GET("/items/:itemId", getItemById)
 	e.GET("/image/:imageFilename", getImg)
 
 	// Start server
