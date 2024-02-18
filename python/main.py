ecb8f579ae74dcebd7c3fa89721adda2ac9fc624
秋山瑞季 <g2020501@is.ocha.ac.jp>
2024-02-18 16:03:52 +0900

    Update main.py

--- python/main.py
+++ python/main.py
@@ -1,4 +1,5 @@
 import os
+import json
 import logging
 import pathlib
 from fastapi import FastAPI, Form, HTTPException
@@ -9,6 +10,7 @@ app = FastAPI()
 logger = logging.getLogger("uvicorn")
 logger.level = logging.INFO
 images = pathlib.Path(__file__).parent.resolve() / "images"
+items_file = pathlib.Path(__file__).parent.resolve() / "items.json"
 origins = [os.environ.get("FRONT_URL", "http://localhost:3000")]
 app.add_middleware(
     CORSMiddleware,
@@ -25,9 +27,27 @@ def root():
 
 
 @app.post("/items")
-def add_item(name: str = Form(...)):
+def add_item(name: str = Form(...),category: str = Form(...)):
     logger.info(f"Receive item: {name}")
+    logger.info(f"Receive item: {category}")
+    save_items_to_file(name,category)
     return {"message": f"item received: {name}"}
+
+
+# 新しい商品をjsonファイルに保存する。 {"items": [{"name": "jacket", "category": "fashion"}, ...]}
+def save_items_to_file(name,category):
+    new_item = {"name": name, "category": category}
+    if os.path.exists(items_file):
+        with open(items_file) as f:
+            now_data = json.load(items_file)
+        now_data["items"].append(new_item)
+        with open(items_file, 'w') as f:
+            json.dump(now_data, f, indent=2)
+    else:
+        first_item = {"items": [new_item]}
+        with open(items_file, 'w') as f:
+            json.dump(first_item, f, indent=2)
+
 
 
 @app.get("/image/{image_name}")
