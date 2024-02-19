d0638e55aa63ea4bfd833ebd4289f12f74b3a9ce
Aroduno <antevasin.ryosuke@gmail.com>
2024-02-20 00:30:40 +0900

    add feature to save new item to JSON file when POST request

--- go/app/main.go
+++ go/app/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 	"path"
@@ -13,8 +15,18 @@ import (
 )
 
 const (
-	ImgDir = "images"
+	ImgDir    = "images"
+	ItemsFile = "items.json"
 )
+
+type Item struct {
+	Name     string `json:"name"`
+	Category string `json:"category"`
+}
+
+type Items struct {
+	Items []Item `json:"items"`
+}
 
 type Response struct {
 	Message string `json:"message"`
@@ -25,22 +37,75 @@ func root(c echo.Context) error {
 	return c.JSON(http.StatusOK, res)
 }
 
+func readItemsFromFile() (*Items, error) {
+	file, err := os.Open(ItemsFile)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return &Items{Items: []Item{}}, nil
+		}
+		return nil, err
+	}
+	defer file.Close()
+
+	var items Items
+	// JSON to Items
+	decoder := json.NewDecoder(file)
+	if err := decoder.Decode(&items); err != nil {
+		if err == io.EOF {
+			return &Items{Items: []Item{}}, nil
+		}
+		return nil, err
+	}
+
+	return &items, nil
+}
+
+func writeItemsToFile(items *Items) error {
+	file, err := os.Create(ItemsFile)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	// Items to JSON
+	encoder := json.NewEncoder(file)
+	if err := encoder.Encode(items); err != nil {
+		return err
+	}
+	return nil
+}
+
 func addItem(c echo.Context) error {
-	// Get form data
 	name := c.FormValue("name")
+	category := c.FormValue("category")
+
+	newItem := Item{Name: name, Category: category}
+
+	// Read existing items from file
+	items, err := readItemsFromFile()
+	if err != nil && !os.IsNotExist(err) {
+		return c.JSON(http.StatusInternalServerError, Response{Message: "Failed to read items from file"})
+	}
+	if items == nil {
+		items = &Items{}
+	}
+
+	// Add new item to items list
+	items.Items = append(items.Items, newItem)
+
+	// Write updated items back to file
+	if err := writeItemsToFile(items); err != nil {
+		return c.JSON(http.StatusInternalServerError, Response{Message: "Failed to write item to file"})
+	}
+
 	c.Logger().Infof("Receive item: %s", name)
 
-	message := fmt.Sprintf("item received: %s", name)
+	message := fmt.Sprintf("Item : %s, Category: %s", newItem.Name, newItem.Category)
 	res := Response{Message: message}
 
-	// http.StatusCreated(201) is also good choice.StatusOK
-  // but in that case, you need to implement and return a URL
-  //   that returns information on the posted item.
 	return c.JSON(http.StatusOK, res)
 }
 
 func getImg(c echo.Context) error {
-	// Create image path
 	imgPath := path.Join(ImgDir, c.Param("imageFilename"))
 
 	if !strings.HasSuffix(imgPath, ".jpg") {
@@ -61,7 +126,6 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 	e.Logger.SetLevel(log.INFO)
-
 	frontURL := os.Getenv("FRONT_URL")
 	if frontURL == "" {
 		frontURL = "http://localhost:3000"
@@ -75,7 +139,6 @@ func main() {
 	e.GET("/", root)
 	e.POST("/items", addItem)
 	e.GET("/image/:imageFilename", getImg)
-
 
 	// Start server
 	e.Logger.Fatal(e.Start(":9000"))
