c6d3ff8e5a03afbe3a87a26d7fe89d183ddae94e
70ru5 <m.tamaki618@gmail.com>
2024-02-19 14:56:59 +0900

    step 3-2

--- go/app/main.go
+++ go/app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
@@ -20,20 +21,78 @@ type Response struct {
 	Message string `json:"message"`
 }
 
+type Item struct {
+	Name     string `json:"name"`
+	Category string `json:"category"`
+}
+
 func root(c echo.Context) error {
-	res := Response{Message: "Hello, world!"}
+	res := Response{Message: "Hello, world!!"}
 	return c.JSON(http.StatusOK, res)
 }
 
 func addItem(c echo.Context) error {
 	// Get form data
 	name := c.FormValue("name")
+	category := c.FormValue("category")
 	c.Logger().Infof("Receive item: %s", name)
+
+	item := Item{Name: name, Category: category}
+
+	err := saveItem(item)
+	if err != nil {
+		return err
+	}
 
 	message := fmt.Sprintf("item received: %s", name)
 	res := Response{Message: message}
 
 	return c.JSON(http.StatusOK, res)
+}
+
+func saveItem(item Item) error {
+	data, err := os.ReadFile("items.json")
+	if err != nil {
+		return err
+	}
+
+	var items struct {
+		Items []Item `json:"items"`
+	}
+
+	err = json.Unmarshal(data, &items)
+	if err != nil {
+		return err
+	}
+
+	items.Items = append(items.Items, item)
+
+	newData, err := json.Marshal(items)
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile("items.json", newData, 0644)
+	return err
+
+}
+
+func getItems(c echo.Context) error {
+	data, err := os.ReadFile("items.json")
+	if err != nil {
+		return err
+	}
+
+	var items struct {
+		Items []Item `json:"items"`
+	}
+
+	err = json.Unmarshal(data, &items)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, items)
 }
 
 func getImg(c echo.Context) error {
@@ -71,8 +130,8 @@ func main() {
 	// Routes
 	e.GET("/", root)
 	e.POST("/items", addItem)
+	e.GET("/items", getItems)
 	e.GET("/image/:imageFilename", getImg)
-
 
 	// Start server
 	e.Logger.Fatal(e.Start(":9000"))
