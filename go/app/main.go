0274636c8be32736145a183de5146a0e50cef3d0
F-0728 <fugue2767@icloud.com>
2024-02-19 11:29:36 +0900

    3-3: saving json file

--- go/app/main.go
+++ go/app/main.go
@@ -20,6 +20,13 @@ type Response struct {
 	Message string `json:"message"`
 }
 
+type Item struct {
+	Name     string `json:"name"`
+	Category string `json:"category"`
+}
+
+type Items []Item
+
 func root(c echo.Context) error {
 	res := Response{Message: "Hello, world!"}
 	return c.JSON(http.StatusOK, res)
@@ -30,10 +37,51 @@ func addItem(c echo.Context) error {
 	name := c.FormValue("name")
 	c.Logger().Infof("Receive item: %s", name)
 
+	category := c.FormValue("category")
+	c.Logger().Infof("Receive category: %s", category)
+
+	// Open the JSON file
+    jsonFile, err := os.Open("items.json")
+    if err != nil {
+        return err
+    }
+    defer jsonFile.Close()
+
+    // Decode the JSON file into a Go slice
+    var items Items
+    byteValue, _ := ioutil.ReadAll(jsonFile)
+    json.Unmarshal(byteValue, &items)
+
+    // Add the new item to the slice
+    items = append(items, Item{Name: name, Category: category})
+
+    // Encode the slice back into JSON
+    file, _ := json.MarshalIndent(items, "", " ")
+
+    // Write the JSON back to the file
+    _ = ioutil.WriteFile("items.json", file, 0644)
+
 	message := fmt.Sprintf("item received: %s", name)
 	res := Response{Message: message}
 
 	return c.JSON(http.StatusOK, res)
+}
+
+func getItems(c echo.Context) error {
+    // Open the JSON file
+    jsonFile, err := os.Open("items.json")
+    if err != nil {
+        return err
+    }
+    defer jsonFile.Close()
+
+    // Decode the JSON file into a Go slice
+    var items Items
+    byteValue, _ := ioutil.ReadAll(jsonFile)
+    json.Unmarshal(byteValue, &items)
+
+    // Return the slice
+    return c.JSON(http.StatusOK, items)
 }
 
 func getImg(c echo.Context) error {
@@ -71,6 +119,7 @@ func main() {
 	// Routes
 	e.GET("/", root)
 	e.POST("/items", addItem)
+	e.GET("/items", getItems)
 	e.GET("/image/:imageFilename", getImg)
 
 
