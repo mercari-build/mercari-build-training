56bf908cd7215956c49e754a43f7f7466540696b
Yoshino <nocchi3695@gmail.com>
2024-02-20 16:29:29 +0900

    STEP3

--- go/app/main.go
+++ go/app/main.go
@@ -6,11 +6,18 @@ import (
 	"os"
 	"path"
 	"strings"
+	"encoding/json"
+	"io/ioutil"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/gommon/log"
 )
+
+type Item struct {
+	Name     string `json:"name"`
+	Category string `json:"category"`
+}
 
 const (
 	ImgDir = "images"
@@ -19,6 +26,7 @@ const (
 type Response struct {
 	Message string `json:"message"`
 }
+
 
 func root(c echo.Context) error {
 	res := Response{Message: "Hello, world!"}
@@ -51,6 +59,42 @@ func getImg(c echo.Context) error {
 	return c.File(imgPath)
 }
 
+func readItemsFromFile(filename string) ([]Item, error) {
+	// ファイルをオープン
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Errorf("Error opening file: %s", err)
+		return nil, err
+	}
+	defer f.Close()
+
+	// ファイルの内容を読み取り
+	bytes, err := ioutil.ReadAll(f)
+	if err != nil {
+		log.Errorf("Error reading file: %s", err)
+		return nil, err
+	}
+
+	// JSONデコード
+	var items []Item
+	err = json.Unmarshal(bytes, &items)
+	if err != nil {
+		log.Errorf("Error decoding JSON: %s", err)
+		return nil, err
+	}
+
+	return items, nil
+}
+
+func getItems(c echo.Context) error {
+	items, err := readItemsFromFile("items.json")
+	if err != nil {
+		errMsg := fmt.Sprintf("Internal Server Error: %s", err) // エラーメッセージを作成
+		fmt.Println(errMsg) // エラーメッセージを出力
+		return c.JSON(http.StatusInternalServerError, Response{Message: errMsg})
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{"items": items})
+}
 func main() {
 	e := echo.New()
 
@@ -70,6 +114,7 @@ func main() {
 
 	// Routes
 	e.GET("/", root)
+	e.GET("/items", getItems)
 	e.POST("/items", addItem)
 	e.GET("/image/:imageFilename", getImg)
 
