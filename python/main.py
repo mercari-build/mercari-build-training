25b9656b3191bb6068b93889b85896821d61e6be
Em <mgfe2sio4.7@gmail.com>
2024-02-20 14:42:43 +0000

    Still debugging STEP 3

--- python/main.py
+++ python/main.py
@@ -33,33 +33,37 @@ def get_items():
 
 @app.post("/items")
 async def add_item(name: str = Form(...), category: str = Form(...), image: UploadFile = File(...)):
+    try:
+        #Hash
+        image_bytes = await image.read()
+        image_hash = hashlib.sha256(image_bytes).hexdigest()
 
-    #Hash
-    image_bytes = await image.read()
-    image_hash = hashlib.sha256(image_bytes).hexdigest()
+        image_name = f"{image_hash}.jpg"
+        with open(images / image_name, 'wb') as f:
+            f.write(image_bytes)
 
-    image_name = f"{image_hash}.jpg"
-    with open(images / image_name, 'wb') as f:
-        f.write(image_bytes)
+        # Open the JSON file
+        with open('items.json', 'r') as f:
+            items_data = json.load(f)
+        
+        #Append the new item
+        items_data["items"].append({
+            'name': name,
+            'category': category,
+            'image_name':image_name
+        })
 
-    # Open the JSON file
-    with open('items.json', 'r') as f:
-        items_data = json.load(f)
-    
-    #Append the new item
-    items_data["items"].append({
-        'name': name,
-        'category': category,
-        'image_name':image_name
-    })
+        #Write the updates to items.json
+        with open('items.json', 'w') as f:
+            json.dump(items_data, f)
 
-    #Write the updates to items.json
-    with open('items.json', 'w') as f:
-        json.dump(items_data, f)
+        logger.info(f"Receive item: {name}, category: {category}, image: {image_name}")
 
-    logger.info(f"Receive item: {name}, category: {category}, image: {image_name}")
+        return {"message": f"item received: {name}, Category: {category}"}
+    except Exception:
+        logger.error(f"An unexpected error occured")
+        raise HTTPException(status_code=500, detail="Internal Server Error")
 
-    return {"message": f"item received: {name}, Category: {category}"}
 
 
 @app.get("/image/{image_name}")
@@ -69,11 +73,11 @@ async def get_image(image_name):
     image_path = images / image_name
 
     if not image_name.endswith(".jpg"):
-        logger.debug(f"Image path does not end with .jpg")
+        logger.error(f"Image path does not end with .jpg")
         raise HTTPException(status_code=400, detail="Image path does not end with .jpg")
 
     elif not image_path.exists():
-        logger.debug(f"Image not found: {image_name}")
+        logger.error(f"Image not found: {image_name}")
         return RedirectResponse(url="/image/default.jpg")
 
     return FileResponse(image_path)
