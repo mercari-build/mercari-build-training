7d99aaa404a6ee817d21126957dd515a03fc23dc
Em <mgfe2sio4.7@gmail.com>
2024-02-20 14:01:53 +0000

    Trying to fix STEP3

--- python/main.py
+++ python/main.py
@@ -1,11 +1,12 @@
 import os
+import hashlib
 import json
 import logging
 import pathlib
-from fastapi import FastAPI, Form, HTTPException, UploadFile, File
+from fastapi import FastAPI, File, Form, HTTPException, UploadFile
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
-import hashlib
+from starlette.responses import RedirectResponse
 
 app = FastAPI()
 logger = logging.getLogger("uvicorn")
@@ -20,7 +21,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
 @app.get("/")
 def root():
     return {"message": "Hello, world!"}
@@ -33,15 +33,13 @@ def get_items():
 
 @app.post("/items")
 async def add_item(name: str = Form(...), category: str = Form(...), image: UploadFile = File(...)):
-    logger.info(f"Receive item: {name}")
 
     #Hash
     image_bytes = await image.read()
     image_hash = hashlib.sha256(image_bytes).hexdigest()
 
     image_name = f"{image_hash}.jpg"
-    image_dir = os.getcwd() / "images"
-    with open(image_dir / image_name, 'wb') as f:
+    with open(images / image_name, 'wb') as f:
         f.write(image_bytes)
 
     # Open the JSON file
@@ -51,7 +49,7 @@ async def add_item(name: str = Form(...), category: str = Form(...), image: Uplo
     #Append the new item
     items_data["items"].append({
         'name': name,
-        'category': category
+        'category': category,
         'image_name':image_name
     })
 
@@ -59,29 +57,39 @@ async def add_item(name: str = Form(...), category: str = Form(...), image: Uplo
     with open('items.json', 'w') as f:
         json.dump(items_data, f)
 
+    logger.info(f"Receive item: {name}, category: {category}, image: {image_name}")
+
     return {"message": f"item received: {name}, Category: {category}"}
 
 
 @app.get("/image/{image_name}")
 async def get_image(image_name):
+    logger.info(f"Receive image: {image_name}")
     # Create image path
-    image = images / image_name
+    image_path = images / image_name
 
     if not image_name.endswith(".jpg"):
+        logger.debug(f"Image path does not end with .jpg")
         raise HTTPException(status_code=400, detail="Image path does not end with .jpg")
 
-    if not image.exists():
-        logger.debug(f"Image not found: {image}")
-        image = images / "default.jpg"
+    elif not image_path.exists():
+        logger.debug(f"Image not found: {image_name}")
+        return RedirectResponse(url="/image/default.jpg")
 
-    return FileResponse(image)
+    return FileResponse(image_path)
 
-@app.get("/items/<item_id")
+@app.get("/items/{item_id}")
 def get_item(item_id: int):
     try:
         with open('items.json', 'r') as f:
             items_data = json.load(f)
-        item = items_data[item_id + 1]
-        return item
-    except IndexError:
-        raise HTTPException(status_code=404, detail="Item not found")
+        if 0 <= item_id < len(items_data["items"]):
+            item = items_data["items"][item_id + 1]
+            logger.info(f"Access item: {item_id}")
+            return item
+        else:
+            logger.error(f"Invalid item ID: {item_id}")
+            raise HTTPException(status_code=404, detail="Item not found (Invalid ID)")
+    except FileNotFoundError: #if items.json is not found
+        logger.error(f"File not found")
+        raise HTTPException(status_code=500, detail="Internal Server Error")
