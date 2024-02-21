5661a3463631ab2e6137ea0cc240dba28dae8fcf
xintong <xintong000927@gmail.com>
2024-02-21 12:40:32 +0800

    step3

--- python/main.py
+++ python/main.py
@@ -1,9 +1,12 @@
 import os
 import logging
 import pathlib
-from fastapi import FastAPI, Form, HTTPException
+from fastapi import FastAPI, Form, HTTPException, UploadFile, File
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import json
+import hashlib
 
 app = FastAPI()
 logger = logging.getLogger("uvicorn")
@@ -23,12 +26,35 @@ app.add_middleware(
 def root():
     return {"message": "Hello, world!"}
 
+@app.get("/items/{item_id}")
+def get_itemsById(item_id: int):
+    with open('items.json','r') as f:
+        data = json.load(f)
+    if item_id >= len(data["items"]):
+        raise HTTPException(status_code=404, detail="Array out of bound")
+    return data[item_id]
+
+@app.get("/items")
+def get_items():
+    with open('items.json','r') as f:
+        data = json.load(f)
+    return data
 
 @app.post("/items")
-def add_item(name: str = Form(...)):
+async def add_item(name: str = Form(...), category: str = Form(...), file: UploadFile = File(...) ):
     logger.info(f"Receive item: {name}")
+    read = await file.read()
+    hash = hashlib.sha256(read).hexdigest()
+    #save image
+    with open(os.path.join("images",f"{hash}.jpg"),'wb') as f:
+        f.write(read)
+    dict = {"name":name, "category":category, "image_filename": f"{hash}.jpg"}
+    with open('items.json','r') as f_r:
+        data = json.load(f_r)
+    data["items"].append(dict)
+    with open('items.json','w') as f_w:
+        json.dump(data, f_w)
     return {"message": f"item received: {name}"}
-
 
 @app.get("/image/{image_name}")
 async def get_image(image_name):
@@ -43,3 +69,6 @@ async def get_image(image_name):
         image = images / "default.jpg"
 
     return FileResponse(image)
+
+if __name__ == '__main__':
+    uvicorn.run("main:app",port=9000,reload=True)
