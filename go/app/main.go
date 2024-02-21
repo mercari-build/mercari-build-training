bfc02da2cfcfe3f625f5f46d8affa1e65c585835
2003ma <amemomo00momo@gmail.com>
2024-02-21 09:34:02 +0900

    5の実装&idを自動追加

--- go/app/main.go
+++ go/app/main.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	"crypto/sha256"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
 	"path"
+	"strconv"
 	"strings"
-
-	"encoding/hex"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -29,7 +29,12 @@ type itemResponse struct {
 	Items []Item `json:"items"`
 }
 
+type itemsResponse struct {
+	Item Item `json:"item"`
+}
+
 type Item struct {
+	ID       int    `json:"id"`
 	Name     string `json:"name"`
 	Category string `json:"category"`
 	Image    string `json:"image"`
@@ -45,10 +50,12 @@ func addItem(c echo.Context) error {
 	name := c.FormValue("name")
 	category := c.FormValue("category")
 	image := c.FormValue("image")
+
 	c.Logger().Infof("Receive item: %s", name)
 	c.Logger().Infof("Receive category: %s", category)
 	c.Logger().Infof("Receive image: %s", image)
 
+	//image
 	img, err := os.Open(image)
 	if err != nil {
 		log.Fatal(err)
@@ -69,10 +76,6 @@ func addItem(c echo.Context) error {
 	hashValue := hash.Sum(nil)
 	// Convert the byte slice to a hex-encoded string
 	hashString := hex.EncodeToString(hashValue)
-	item := Item{name, category, string(hashString)}
-
-	message := fmt.Sprintf("item received: %s in %s category", item.Name, item.Category)
-	res := Response{Message: message}
 
 	file1, err := os.Open("item.json") //すでにあるファイルを開く
 	if err != nil {
@@ -86,7 +89,29 @@ func addItem(c echo.Context) error {
 	var itemslice []Item
 	json.Unmarshal(jsonData, &itemslice)
 
-	itemslice = append(itemslice, item)
+	//Get max ID
+	maxID := 0
+	for _, item := range itemslice {
+		if item.ID > maxID {
+			maxID = item.ID
+		}
+	}
+
+	// Create a new item with the next ID
+	newItem := Item{
+		ID:       maxID + 1,
+		Name:     name,
+		Category: category,
+		Image:    hashString,
+	}
+
+	//Print message
+	message := fmt.Sprintf("item received: %s in %s category", newItem.Name, newItem.Category)
+	res := Response{Message: message}
+
+	// Append the new item to the slice
+	itemslice = append(itemslice, newItem)
+
 	file2, err := os.Create("item.json") // fileはos.File型
 	if err != nil {
 		log.Fatal(err)
@@ -112,6 +137,47 @@ func getItem(c echo.Context) error {
 
 	res := itemResponse{Items: itemslice}
 
+	return c.JSON(http.StatusOK, res)
+}
+
+func getItems(c echo.Context) error {
+	itemIDStr := c.Param("item_id")
+
+	itemID, err := strconv.Atoi(itemIDStr)
+	if err != nil {
+		// エラーが発生した場合の処理
+		fmt.Println("item_idを整数に変換できません:", err)
+	}
+
+	fmt.Println(itemID)
+
+	file1, err := os.Open("item.json") //すでにあるファイルを開く
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file1.Close()
+	jsonData, err := ioutil.ReadAll(file1)
+	if err != nil {
+		fmt.Println("JSONデータを読み込めません", err)
+	}
+	var itemslice []Item
+	json.Unmarshal(jsonData, &itemslice)
+	fmt.Println(itemslice)
+
+	var foundItem *Item
+	for _, item := range itemslice {
+		if item.ID == itemID {
+			foundItem = &item
+			break
+		}
+	}
+
+	if foundItem == nil {
+		// 該当する商品が見つからなかった場合の処理
+		return c.JSON(http.StatusNotFound, Response{Message: "Item not found"})
+	}
+
+	res := itemsResponse{Item: *foundItem}
 	return c.JSON(http.StatusOK, res)
 }
 
@@ -152,6 +218,7 @@ func main() {
 	e.GET("/items", getItem)
 	e.POST("/items", addItem)
 	e.GET("/image/:imageFilename", getImg)
+	e.GET("/items/:item_id", getItems)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":9000"))
