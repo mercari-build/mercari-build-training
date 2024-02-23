7489cce49d0841249303ec3af797d217b5dc1bd8
miamakino <mia.makino@gmail.com>
2024-02-22 22:15:16 -0800

    refactor: table normalization for categories chore: .gitignore to ignore pycache

--- python/main.py
+++ python/main.py
@@ -5,6 +5,7 @@ import hashlib
 from fastapi import FastAPI, Form, HTTPException, UploadFile
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
+import sqlite3
 
 app = FastAPI()
 logger = logging.getLogger("uvicorn")
@@ -19,22 +20,58 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-items = list()
+'''
+global variables
+database_file : sqlite3 database file location.
+
+Notes: given sqlite module version is not thread safe according to sqlite3.threadsafety.
+'''
+database_file = "/Users/miamakino/Mercari/mercari-build-training/db/mercari.sqlite3"
 
 @app.get("/")
 def root():
     return {"message": "Hello, world!"}
 
+def get_items_from_db():
+    global database_file
+    con = sqlite3.connect(database_file)
+    cur = con.cursor()
+    res = cur.execute("SELECT items.id, name, category, image_name FROM items inner join categories on items.category_id = categories.id")
+    items = res.fetchall()
+    cur.close()
+    con.close()
+    return items
+
 @app.get("/items")
 def get_items():
+    items = get_items_from_db()
     return {"items": items}
 
 @app.get("/items/{item_id}")
 def get_item(item_id):
-    return items[int (item_id)]
+    global database_file
+    con = sqlite3.connect(database_file)
+    cur = con.cursor()
+    res = cur.execute("SELECT items.id, name, category, image_name FROM items inner join categories on items.category_id WHERE items.id = ?", [int(item_id)])
+    item = res.fetchone()
+    cur.close()
+    con.close()
+    return item
+
+@app.get("/search")
+def search_items(keyword: str):
+    global database_file
+    con = sqlite3.connect(database_file)
+    cur = con.cursor() 
+    res = cur.execute("SELECT items.id, name, category, image_name FROM items INNER JOIN categories ON items.category_id = categories.id WHERE name LIKE ? OR category LIKE ?", [keyword, keyword])
+    found_items = res.fetchall()
+    cur.close()
+    con.close()
+    return found_items
+    
 
 @app.post("/items")
-def add_item(name: str = Form(...), category: str = Form(...), image: UploadFile = Form(...)):
+def add_item(name: str = Form(...), category_id: int = Form(...), image: UploadFile = Form(...)):
     logger.info(f"Receive item: {name}")
 
     file_content = image.file.read()
@@ -42,10 +79,16 @@ def add_item(name: str = Form(...), category: str = Form(...), image: UploadFile
 
     image_hash = hashlib.sha256(file_content).hexdigest()
     save_image(file_content, f"{image_hash}.jpg")
-        
-    new_item = {"name": name, "category": category, "image": f"{image_hash}.jpg"}
-    items.append(new_item)
 
+    global database_file
+    con = sqlite3.connect(database_file)
+    cur = con.cursor()
+    data = [name, category_id, f"{image_hash}.jpg"]
+    res = cur.execute("INSERT INTO items VALUES(NULL,?,?,?)", data)
+    con.commit()
+    items = get_items_from_db()
+    cur.close()
+    con.close()
     return {"items": items}
 
 def save_image (file_content, hashed_filename):
