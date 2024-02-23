c683718ddb3ca9128509541c90feabae8bb037ad
miamakino <103288486+miamakino@users.noreply.github.com>
2024-02-22 19:39:46 -0800

    Revert "STEP 3"

--- sql/records.sql
+++
@@ -1,4 +0,0 @@
-insert into categories(id, category) values (1, 'fashion');
-insert into categories(id, category) values (2, 'book');
-insert into categories(id, category) values (3, 'stationery');
-insert into categories(id, category) values (4, 'food');
