db527dc35fa1781fa1f0bdc094423a73d8f4b823
task4233 <29667656+task4233@users.noreply.github.com>
2025-02-25 08:24:53 +0900

    change(go): revise go code

--- go/app/main.go
+++ go/app/main.go
@@ -1,98 +1,226 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
+	"log/slog"
 	"net/http"
 	"os"
 	"path/filepath"
 	"strings"
-
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
-	"github.com/labstack/gommon/log"
 )
 
 const (
-	imgDir = "images"
-	port   = "9000"
+	imageDirPath     = "images"
+	itemJSONFilePath = "items.json"
+	port             = "9000"
 )
 
-type Response struct {
+var errImageNotFound = errors.New("image not found")
+
+type Handlers struct {
+	// imgDirPath is the path to the directory storing images.
+	imgDirPath string
+	itemRepo   ItemRepository
+}
+
+type HelloResponse struct {
 	Message string `json:"message"`
 }
 
-// hello is an endpoint to return a Hello, world! message.
-func hello(c echo.Context) error {
-	res := Response{
-		Message: "Hello, world!",
+// Hello is a handler to return a Hello, world! message for GET / .
+func (s *Handlers) Hello(w http.ResponseWriter, r *http.Request) {
+	resp := HelloResponse{Message: "Hello, world!"}
+	err := json.NewEncoder(w).Encode(resp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
-	return c.JSON(http.StatusOK, res)
 }
 
-// addItem is an endpoint to add a new item.
-func addItem(c echo.Context) error {
-	// Get form data
-	name := c.FormValue("name")
-	c.Logger().Infof("Receive item: %s", name)
-
-	message := fmt.Sprintf("item received: %s", name)
-	res := Response{
-		Message: message,
-	}
-
-	// http.StatusCreated(201) is also acceptable.
-	return c.JSON(http.StatusOK, res)
+type AddItemRequest struct {
+	Name string `form:"name"`
 }
 
-// getImage is an endpoint to return an image.
-func getImage(c echo.Context) error {
-	// Build image path
-	imgPath := filepath.Join(imgDir, filepath.Clean(c.Param("imageFilename")))
-	rel, err := filepath.Rel(imgDir, imgPath)
+type AddItemResponse struct {
+	Message string `json:"message"`
+}
+
+// parseAddItemRequest parses and validates the request to add an item.
+func parseAddItemRequest(r *http.Request) (*AddItemRequest, error) {
+	req := &AddItemRequest{
+		Name: r.FormValue("name"),
+	}
+
+	// validate the request
+	if req.Name == "" {
+		return nil, errors.New("name is required")
+	}
+
+	return req, nil
+}
+
+// AddItem is a handler to add a new item for POST /items .
+func (s *Handlers) AddItem(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	req, err := parseAddItemRequest(r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// STEP 3-4: add an implementation to store an image
+
+	item := &Item{Name: req.Name}
+	message := fmt.Sprintf("item received: %#v", item)
+	slog.InfoContext(ctx, message)
+
+	// STEP 3-2: add an implementation to store an image
+	err = s.itemRepo.Insert(ctx, item)
+	if err != nil {
+		slog.ErrorContext(ctx, "failed to store item: ", "error", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	resp := AddItemResponse{Message: message}
+	err = json.NewEncoder(w).Encode(resp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+type GetImageRequest struct {
+	FileName string // path value
+}
+
+// parseGetImageRequest parses and validates the request to get an image.
+func parseGetImageRequest(r *http.Request) (*GetImageRequest, error) {
+	req := &GetImageRequest{
+		FileName: r.PathValue("filename"), // from path parameter
+	}
+
+	// validate the request
+	if req.FileName == "" {
+		return nil, errors.New("filename is required")
+	}
+
+	return req, nil
+}
+
+// GetImage is a handler to return an image for GET /images/{filename} .
+func (s *Handlers) GetImage(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	req, err := parseGetImageRequest(r)
+	if err != nil {
+		slog.WarnContext(ctx, "failed to parse get image request: ", "error", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	imgPath, err := s.buildImagePath(req.FileName)
+	if err != nil {
+		// when the image is not found, it returns the default image.
+		if errors.Is(err, errImageNotFound) {
+			slog.DebugContext(ctx, "image not found", "filename", imgPath)
+			imgPath = filepath.Join(s.imgDirPath, "default.jpg")
+		} else {
+			slog.WarnContext(ctx, "failed to build image path: ", "error", err)
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+	}
+
+	slog.InfoContext(ctx, "returned image", "path", imgPath)
+	http.ServeFile(w, r, imgPath)
+}
+
+// buildImagePath builds the image path and validates it.
+func (s *Handlers) buildImagePath(imageFileName string) (string, error) {
+	imgPath := filepath.Join(s.imgDirPath, filepath.Clean(imageFileName))
+
+	// to prevent directory traversal attacks
+	rel, err := filepath.Rel(s.imgDirPath, imgPath)
 	if err != nil || strings.HasPrefix(rel, "..") {
-		res := Response{
-			Message: "Invalid image path",
-		}
-		return c.JSON(http.StatusBadRequest, res)
+		return "", fmt.Errorf("invalid image path: %s", imgPath)
 	}
 
-	c.Logger().Info("Image path: ", imgPath)
+	// validate the image path
+	if !strings.HasSuffix(imgPath, ".jpg") && !strings.HasSuffix(imgPath, ".jpeg") {
+		return "", fmt.Errorf("image path does not end with .jpg or .jpeg: %s", imgPath)
+	}
 
-	if !strings.HasSuffix(imgPath, ".jpg") {
-		res := Response{
-			Message: "Image path does not end with .jpg",
-		}
-		return c.JSON(http.StatusBadRequest, res)
+	// check if the image exists
+	_, err = os.Stat(imgPath)
+	if err != nil {
+		return imgPath, errImageNotFound
 	}
-	if _, err := os.Stat(imgPath); err != nil {
-		c.Logger().Debugf("Image not found: %s", imgPath)
-		imgPath = filepath.Join(imgDir, "default.jpg")
-	}
-	return c.File(imgPath)
+
+	return imgPath, nil
+}
+
+type Item struct {
+	ID   int    `db:"id"`
+	Name string `db:"name"`
+}
+
+//go:generate go run go.uber.org/mock/mockgen -source=$GOFILE -package=${GOPACKAGE} -destination=./mock_$GOFILE
+
+// ItemRepository is an interface to manage items.
+type ItemRepository interface {
+	Insert(ctx context.Context, item *Item) error
+}
+
+// itemRepositoryJSON is an implementation of ItemRepository using JSON files.
+type itemRepositoryJSON struct {
+	// fileName is the path to the JSON file storing items.
+	fileName string
+}
+
+// NewItemRepositoryJSON creates a new itemRepositoryJSON.
+func NewItemRepositoryJSON(fileName string) ItemRepository {
+	return &itemRepositoryJSON{fileName: fileName}
+}
+
+// Insert inserts an item into the JSON file.
+func (i *itemRepositoryJSON) Insert(ctx context.Context, item *Item) error {
+	// STEP 3-2: add an implementation to store an image
+
+	return nil
 }
 
 func main() {
-	e := echo.New()
+	// set up logger
+	logger := slog.New(slog.NewJSONHandler(os.Stderr, nil))
+	slog.SetDefault(logger)
+	// STEP 3-6: set the log level to info
+	slog.SetLogLoggerLevel(slog.LevelInfo)
 
-	// Middleware
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-	e.Logger.SetLevel(log.INFO)
-
-	frontURL := os.Getenv("FRONT_URL")
-	if frontURL == "" {
+	// set up CORS settings
+	frontURL, found := os.LookupEnv("FRONT_URL")
+	if !found {
 		frontURL = "http://localhost:3000"
 	}
-	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{frontURL},
-		AllowMethods: []string{http.MethodGet, http.MethodPost},
-	}))
 
-	// Routes
-	e.GET("/", hello)
-	e.POST("/items", addItem)
-	e.GET("/image/:imageFilename", getImage)
+	// set up handlers
+	itemRepo := NewItemRepositoryJSON(itemJSONFilePath)
+	h := &Handlers{imgDirPath: imageDirPath, itemRepo: itemRepo}
 
-	// Start server
-	e.Logger.Fatal(e.Start(":" + port))
+	// set up routes
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /", h.Hello)
+	mux.HandleFunc("POST /items", h.AddItem)
+	mux.HandleFunc("GET /images/{filename}", h.GetImage)
+
+	// start the server
+	slog.Info("http server started on", "port", port)
+	err := http.ListenAndServe(":"+port, simpleCORSMiddleware(simpleLoggerMiddleware(mux), frontURL, []string{"GET", "HEAD", "POST", "OPTIONS"}))
+	if err != nil {
+		slog.Error("failed to start server: ", "error", err)
+	}
 }
--- go/go.mod
+++ go/go.mod
@@ -1,20 +1,3 @@
 module mercari-build-training
 
-go 1.23.4
-
-require (
-	github.com/labstack/echo/v4 v4.13.3
-	github.com/labstack/gommon v0.4.2
-)
-
-require (
-	github.com/mattn/go-colorable v0.1.14 // indirect
-	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasttemplate v1.2.2 // indirect
-	golang.org/x/crypto v0.32.0 // indirect
-	golang.org/x/net v0.34.0 // indirect
-	golang.org/x/sys v0.29.0 // indirect
-	golang.org/x/text v0.21.0 // indirect
-	golang.org/x/time v0.8.0 // indirect
-)
+go 1.24
