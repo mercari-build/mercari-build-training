3453423751d5eaa37e2382a5bb459028ed9f8f57
nat888888 <gosummerleaf7@gmail.com>
2025-03-01 16:44:18 +0900

    work in progress

--- python/main.py
+++ python/main.py
@@ -7,6 +7,7 @@ from fastapi.middleware.cors import CORSMiddleware
 import sqlite3
 from pydantic import BaseModel
 from contextlib import asynccontextmanager
+import json
 
 
 # Define the path to the images & sqlite3 database
@@ -69,13 +70,17 @@ class AddItemResponse(BaseModel):
 @app.post("/items", response_model=AddItemResponse)
 def add_item(
     name: str = Form(...),
+    category: str = Form(...),
     db: sqlite3.Connection = Depends(get_db),
 ):
     if not name:
         raise HTTPException(status_code=400, detail="name is required")
+    if not category:
+        raise HTTPException(status_code=400, detail="category is required")
 
-    insert_item(Item(name=name))
-    return AddItemResponse(**{"message": f"item received: {name}"})
+    insert_item(Item(name=name,category=category))
+    return AddItemResponse(**{"message": f"item received: {name}, category: {category}"})
+
 
 
 # get_image is a handler to return an image for GET /images/{filename} .
@@ -96,8 +101,21 @@ async def get_image(image_name):
 
 class Item(BaseModel):
     name: str
+    category: str#これなに
 
 
 def insert_item(item: Item):
-    # STEP 4-1: add an implementation to store an item
+    with open('items.json') as f:
+        d_update = json.load(f)
+    
+    item_detail = {
+                "name": item.name,
+                "category": item.category
+            }
+    
+    d_update['items'].append(item_detail)
+
+    with open('items.json', 'w') as f:
+        json.dump(d, f, indent=2)
+
     pass
