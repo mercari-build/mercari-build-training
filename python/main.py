b70d5dfc5b0f4c7430420e75815de8921c0fee60
Yuki Yada <57289763+YadaYuki@users.noreply.github.com>
2025-03-01 12:05:51 +0900

    change(python): update python server code (#269)
    ## What
    
    - SSIA
    - https://github.com/mercari-build/mercari-build-training/pull/262
    
    ## CHECKS :warning:
    
    Please make sure you are aware of the following.
    
    - [ ] **The changes in this PR doesn't have private information

--- python/main.py
+++ python/main.py
@@ -1,11 +1,44 @@
 import os
 import logging
 import pathlib
-from fastapi import FastAPI, Form, HTTPException
+from fastapi import FastAPI, Form, HTTPException, Depends
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
+import sqlite3
+from pydantic import BaseModel
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+
+# Define the path to the images & sqlite3 database
+images = pathlib.Path(__file__).parent.resolve() / "images"
+db = pathlib.Path(__file__).parent.resolve() / "db" / "mercari.sqlite3"
+
+
+def get_db():
+    if not db.exists():
+        yield
+
+    conn = sqlite3.connect(db)
+    conn.row_factory = sqlite3.Row  # Return rows as dictionaries
+    try:
+        yield conn
+    finally:
+        conn.close()
+
+
+# STEP 5-1: set up the database connection
+def setup_database():
+    pass
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    setup_database()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
 logger = logging.getLogger("uvicorn")
 logger.level = logging.INFO
 images = pathlib.Path(__file__).parent.resolve() / "images"
@@ -19,17 +52,34 @@ app.add_middleware(
 )
 
 
-@app.get("/")
-def root():
-    return {"message": "Hello, world!"}
+class HelloResponse(BaseModel):
+    message: str
 
 
-@app.post("/items")
-def add_item(name: str = Form(...)):
-    logger.info(f"Receive item: {name}")
-    return {"message": f"item received: {name}"}
+@app.get("/", response_model=HelloResponse)
+def hello():
+    return HelloResponse(**{"message": "Hello, world!"})
 
 
+class AddItemResponse(BaseModel):
+    message: str
+
+
+# add_item is a handler to add a new item for POST /items .
+@app.post("/items", response_model=AddItemResponse)
+def add_item(
+    name: str = Form(...),
+    category: str = Form(...),
+    db: sqlite3.Connection = Depends(get_db),
+):
+    if not name:
+        raise HTTPException(status_code=400, detail="name is required")
+
+    insert_item(Item(name=name, category=category))
+    return AddItemResponse(**{"message": f"item received: {name}"})
+
+
+# get_image is a handler to return an image for GET /images/{filename} .
 @app.get("/image/{image_name}")
 async def get_image(image_name):
     # Create image path
@@ -43,3 +93,13 @@ async def get_image(image_name):
         image = images / "default.jpg"
 
     return FileResponse(image)
+
+
+class Item(BaseModel):
+    name: str
+    category: str
+
+
+def insert_item(item: Item):
+    # STEP 4-1: add an implementation to store an item
+    pass
