0ae82f3e57be45099feed77757e972a1ed0466b0
Asasui2440 <nekocat3838@gmail.com>
2025-03-04 23:05:43 +0900

    server.goとinfra.goを追加

--- go/app/infra.go
+++ go/app/infra.go
@@ -2,7 +2,12 @@ package app
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"sync"
 	// STEP 5-1: uncomment this line
 	// _ "github.com/mattn/go-sqlite3"
 )
@@ -10,8 +15,10 @@ import (
 var errImageNotFound = errors.New("image not found")
 
 type Item struct {
-	ID   int    `db:"id" json:"-"`
-	Name string `db:"name" json:"name"`
+	ID       int    `db:"id" json:"-"`
+	Name     string `db:"name" json:"name"`
+	Category string `db:"category" json:"category"`
+	Image    string `db:"image" json:"image"`
 }
 
 // Please run `go generate ./...` to generate the mock implementation
@@ -20,30 +27,112 @@ type Item struct {
 //go:generate go run go.uber.org/mock/mockgen -source=$GOFILE -package=${GOPACKAGE} -destination=./mock_$GOFILE
 type ItemRepository interface {
 	Insert(ctx context.Context, item *Item) error
+	GetAll(ctx context.Context) ([]Item, error)
+	GetByID(ctx context.Context, id int) (*Item, error)
 }
 
 // itemRepository is an implementation of ItemRepository
 type itemRepository struct {
 	// fileName is the path to the JSON file storing items.
 	fileName string
+	mu       sync.Mutex
 }
 
 // NewItemRepository creates a new itemRepository.
 func NewItemRepository() ItemRepository {
-	return &itemRepository{fileName: "items.json"}
+	return &itemRepository{fileName: "/Users/sui/mercari-build-training/go/items.json"}
+
+}
+
+func (i *itemRepository) GetByID(ctx context.Context, id int) (*Item, error) {
+	items, err := i.GetAll(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	if id < 0 || id >= len(items) {
+		return nil, fmt.Errorf("item not found")
+	}
+	return &items[id], nil
 }
 
 // Insert inserts an item into the repository.
 func (i *itemRepository) Insert(ctx context.Context, item *Item) error {
-	// STEP 4-1: add an implementation to store an item
+	// STEP 4-2: add an implementation to store an item
+	i.mu.Lock()
+	defer i.mu.Unlock()
 
+	items, err := i.GetAll(ctx)
+	if err != nil {
+		return err
+	}
+	item.ID = len(items)
+
+	data, err := os.ReadFile(i.fileName)
+	if err != nil {
+		if os.IsNotExist(err) {
+			data = []byte(`{"items": []}`)
+		} else {
+			return fmt.Errorf("failed to read file: %w", err)
+		}
+	}
+
+	var jsonData struct {
+		Items []Item `json:"items"`
+	}
+	if err := json.Unmarshal(data, &jsonData); err != nil {
+		return fmt.Errorf("failed to parse JSON: %w", err)
+	}
+
+	//add ID
+	jsonData.Items = append(jsonData.Items, *item)
+
+	//parse JSON to struct
+	newData, err := json.MarshalIndent(jsonData, "", "  ")
+	if err != nil {
+		return fmt.Errorf("failed to encode JSON: %w", err)
+	}
+
+	//save to JSON files
+	if err := os.WriteFile(i.fileName, newData, 0644); err != nil {
+		return fmt.Errorf("failed to write file: %w", err)
+	}
+	//write lastID to JSON
 	return nil
+
 }
 
 // StoreImage stores an image and returns an error if any.
 // This package doesn't have a related interface for simplicity.
 func StoreImage(fileName string, image []byte) error {
 	// STEP 4-4: add an implementation to store an image
+	imageDirPath := "/Users/sui/mercari-build-training/go/images"
+	filePath := filepath.Join(imageDirPath, fileName)
 
+	//save file to the filepath
+	err := os.WriteFile(filePath, image, 0644)
+	if err != nil {
+		return fmt.Errorf("failed to save image: %w", err)
+	}
 	return nil
+}
+
+func (i *itemRepository) GetAll(ctx context.Context) ([]Item, error) {
+	//read content from file as data(string)
+	data, err := os.ReadFile(i.fileName)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read file: %w", err)
+
+	}
+	//content of struct
+	var jsonData struct {
+		Items []Item `json:"items"`
+	}
+
+	//parse JSON to struct(change data to jsonData(struct))
+	if err := json.Unmarshal(data, &jsonData); err != nil {
+		return nil, fmt.Errorf("failed to parse JSON: %w", err)
+	}
+	//parsed Item list is in jsonData.Items
+	return jsonData.Items, nil
 }
