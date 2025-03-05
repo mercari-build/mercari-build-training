e13e5f1d0d42a0f110c7167fcd31349029634dd1
MayoNakajo <mayonakajo@gmail.com>
2025-03-05 14:46:20 -0500

    Update server.go

--- go/app/server.go
+++ go/app/server.go
@@ -147,10 +147,13 @@ func (s *Handlers) AddItem(w http.ResponseWriter, r *http.Request) {
 
 	item := &Item{
 		Name: req.Name,
+		Category: req.category,
 		// STEP 4-2: add a category field
 		// STEP 4-4: add an image field
 	}
 	message := fmt.Sprintf("item received: %s", item.Name)
+	// DEBUG
+	message := fmt.Sprintf("item category: %s", item.Category)
 	slog.Info(message)
 
 	// STEP 4-2: add an implementation to store an item
