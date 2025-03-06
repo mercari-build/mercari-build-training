46a1b302b1bd85b309a7b460fa0fa3861899696b
rheyuh <r.iai@outlook.com>
2025-03-06 19:15:56 +1100

    list new item, get list of items, add item images, return item details

--- python/main.py
+++ python/main.py
@@ -7,6 +7,8 @@ from fastapi.middleware.cors import CORSMiddleware
 import sqlite3
 from pydantic import BaseModel
 from contextlib import asynccontextmanager
+import json
+import hashlib
 
 
 # Define the path to the images & sqlite3 database
@@ -36,11 +38,13 @@ async def lifespan(app: FastAPI):
     setup_database()
     yield
 
+# TODO: Create items.json if it does not exist
 
 app = FastAPI(lifespan=lifespan)
 
 logger = logging.getLogger("uvicorn")
-logger.level = logging.INFO
+# change logger level to display varying levels of logs displayed on console
+logger.level = logging.DEBUG
 images = pathlib.Path(__file__).parent.resolve() / "images"
 origins = [os.environ.get("FRONT_URL", "http://localhost:3000")]
 app.add_middleware(
@@ -69,13 +73,56 @@ class AddItemResponse(BaseModel):
 @app.post("/items", response_model=AddItemResponse)
 def add_item(
     name: str = Form(...),
+    category: str = Form(...),
+    # Form(None) for non-required fields
+    image: str | None = Form(None),
+    
     db: sqlite3.Connection = Depends(get_db),
 ):
+    print(name, category, image)
     if not name:
         raise HTTPException(status_code=400, detail="name is required")
 
-    insert_item(Item(name=name))
-    return AddItemResponse(**{"message": f"item received: {name}"})
+    if not category:
+        raise HTTPException(status_code=400, detail="category is required")
+
+    if image != None:
+        # print(image)
+        image_hash = hash_image(image)
+
+    insert_item(Item(name=name, category=category, image=image))
+    return AddItemResponse(**{"message": f"item received: name={name}, category={category}, image={image_hash}"})
+
+@app.get("/items")
+def get_items():
+    with open('items.json', 'r') as json_file:
+        try:
+            data = json.load(json_file)
+        except json.JSONDecodeError:
+            data = {}
+    return data
+
+@app.get("/items/{item_id}")
+def get_item(item_id):
+    # check if item_id is a valid integer
+    try:
+        item_id = int(item_id)
+    except:
+        raise HTTPException(status_code=400, detail="Item ID is not a valid number")
+    
+    with open('items.json', 'r') as json_file:
+        try:
+            data = json.load(json_file)
+        except json.JSONDecodeError:
+            data = {}
+
+
+    #TODO: error handling - items might be empty
+    if not data:
+        raise HTTPException(Status_code=400, detail="Item ID does not exist")
+    items = data['items']
+    item = items[item_id - 1]
+    return item
 
 
 # get_image is a handler to return an image for GET /images/{filename} .
@@ -88,6 +135,7 @@ async def get_image(image_name):
         raise HTTPException(status_code=400, detail="Image path does not end with .jpg")
 
     if not image.exists():
+        # changed logger level from debug to higher log level (info, error etc.)
         logger.debug(f"Image not found: {image}")
         image = images / "default.jpg"
 
@@ -96,8 +144,42 @@ async def get_image(image_name):
 
 class Item(BaseModel):
     name: str
+    category: str
+    image: str | None
 
 
 def insert_item(item: Item):
     # STEP 4-2: add an implementation to store an item
-    pass
+    new_data = {
+        "name": item.name,
+        "category": item.category,
+        "image": item.image
+    }
+
+    with open('items.json', 'r') as json_file:
+        try:
+            data = json.load(json_file)
+        except json.JSONDecodeError:
+            data = {}
+
+        # print(data)
+    
+    if 'items' not in data:
+        data['items'] = []
+
+    data['items'].append(new_data)
+
+    # print(data)
+
+    with open('items.json', 'w') as json_file:
+        json.dump(data, json_file, indent=4)
+
+
+def hash_image(image):
+    with open(image, "rb") as f:
+        try:
+            image_bytes = f.read()
+        except:
+            raise HTTPException(status_code=400, detail="Image not found")
+        image_hash = hashlib.sha256(image_bytes).hexdigest()
+    return image_hash
