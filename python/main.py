035de55637d8094756627fa4bf9555ed38e5b731
fariha zahra <fariha.qorinaa@gmail.com>
2025-03-06 19:46:11 +0900

    step 4-4

--- python/main.py
+++ python/main.py
@@ -1,13 +1,14 @@
 import os
 import logging
 import pathlib
-from fastapi import FastAPI, Form, HTTPException, Depends
+from fastapi import FastAPI, Form, HTTPException, Depends, File, UploadFile
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 import sqlite3
 from pydantic import BaseModel
 from contextlib import asynccontextmanager
 import json
+import hashlib
 
 
 # Define the path to the images & sqlite3 database
@@ -68,9 +69,10 @@ class AddItemResponse(BaseModel):
 
 # add_item is a handler to add a new item for POST /items .
 @app.post("/items", response_model=AddItemResponse)
-def add_item(
+async def add_item(
     name: str = Form(...),
-    category: str = Form(...), #image next?
+    category: str = Form(...),
+    image: UploadFile = File(...),
     db: sqlite3.Connection = Depends(get_db),
 ):
     if not name:
@@ -81,8 +83,15 @@ def add_item(
 
     if not category:
         raise HTTPException(status_code=400, detail="category is required")
+    if not image:
+        raise HTTPException(status_code=400, detail="category is required")
+    content = await image.read()
+    hashedimg = hashlib.sha256(content).hexdigest()
+    image_path = images/ f"{hashedimg}.png"
+    with open(image_path, "wb") as a:
+        a.write(content)
 
-    insert_item(Item(name=name, category=category))
+    insert_item(Item(name=name, category=category, image=hashedimg + ".jpg"))
     return AddItemResponse(**{"message": f"item received: {name}"}) 
 
 
@@ -97,9 +106,25 @@ def get_items():
 
     return items
 
+# STEP 4-5 #new
+@app.get("/items/{item_id}")
+def get_item_by_id(item_id: int):
+    #item_idint = int(item_id)
+    if not os.path.exists("items.json"):
+        return []
+
+    with open("items.json", "r") as f:
+        alldata = json.load(f)
+    ###added
+    if not isinstance(alldata, list):
+        return {"error":"Invalid data format in items.json"}
+
+    item= alldata["items"][item_id -1]
+    return alldata
+
 # get_image is a handler to return an image for GET /images/{filename} .
 @app.get("/image/{image_name}")
-async def get_image(image_name):
+async def get_image(image_name: str):
     # Create image path
     image = images / image_name
 
@@ -117,12 +142,13 @@ class Item(BaseModel):
     name: str
     category:str 
     #image
+    image: str
 
 
 def insert_item(item: Item):
     # STEP 4-2: add an implementation to store an item
    # pass
-    item_dict = {"name": item.name, "category": item.category}
+    item_dict = {"name": item.name, "category": item.category, "image": item.image}
 
     if os.path.exists("items.json"):
         with open("items.json", "r") as f:
