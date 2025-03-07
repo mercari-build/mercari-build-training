7c4ecd7f15b336ab6bcfbbea63b8b2dd87e4ec62
yamada3-1990 <k.ae.de19900220+1@gmail.com>
2025-03-07 20:26:42 +0900

    6-3

--- go/app/server_test.go
+++ go/app/server_test.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
@@ -136,6 +137,11 @@ func TestAddItem(t *testing.T) {
 			injector: func(m *MockItemRepository) {
 				// STEP 6-3: define mock expectation
 				// succeeded to insert
+				item := &Item{
+					Name:     "used iPhone 16e",
+					Category: "phone",
+				}
+				m.EXPECT().Insert(gomock.Any(), item).Return(nil)
 			},
 			wants: wants{
 				code: http.StatusOK,
@@ -149,6 +155,11 @@ func TestAddItem(t *testing.T) {
 			injector: func(m *MockItemRepository) {
 				// STEP 6-3: define mock expectation
 				// failed to insert
+				item := &Item{
+					Name:     "used iPhone 16e",
+					Category: "phone",
+				}
+				m.EXPECT().Insert(gomock.Any(), item).Return(fmt.Errorf("insert failed"))
 			},
 			wants: wants{
 				code: http.StatusInternalServerError,
