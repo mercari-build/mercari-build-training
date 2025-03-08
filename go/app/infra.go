d944211ddd6c2bdb8ccab8bae5931b9134e36dcc
Ramya Shalini <ramyashalini2005@gmail.com>
2025-03-08 17:54:25 +0530

    Update infra.go
    Full step 4

--- go/app/infra.go
+++ go/app/infra.go
@@ -3,6 +3,11 @@ package app
 import (
 	"context"
 	"errors"
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+	"crypto/sha256"
 	// STEP 5-1: uncomment this line
 	// _ "github.com/mattn/go-sqlite3"
 )
@@ -13,6 +18,7 @@ type Item struct {
 	ID   int    `db:"id" json:"-"`
 	Name string `db:"name" json:"name"`
 	Category string `db:"category" json:"category"`
+	Image string `db:"image" json:"image"`
 }
 
 // Please run `go generate ./...` to generate the mock implementation
@@ -21,6 +27,8 @@ type Item struct {
 //go:generate go run go.uber.org/mock/mockgen -source=$GOFILE -package=${GOPACKAGE} -destination=./mock_$GOFILE
 type ItemRepository interface {
 	Insert(ctx context.Context, item *Item) error
+	Get(ctx context.Context)([]Item, error)
+	FindID(ctx context.Context, id int)(*Item, error)
 }
 
 // itemRepository is an implementation of ItemRepository
@@ -71,11 +79,68 @@ func (i *itemRepository) Insert(ctx context.Context, item *Item) error {
 
 	return nil
 }
+func (i *itemRepository) Get(ctx context.Context) ([]Item, error) {
+	data, err := os.ReadFile(i.fileName)
+	if err != nil {
+		if os.IsNotExist(err) {
+			// Return empty slice
+			return []Item{}, nil
+		} 
+		return nil, fmt.Errorf("Could not read file, %w", err)
+	}
+
+	var items []Item
+	if len(data) > 0 {
+		err := json.Unmarshal(data, &items)
+		if err != nil {
+			return nil, fmt.Errorf("Error unmarshalling JSON, %w", err)
+		}
+	}
+	return items, nil
+}
+
 
 // StoreImage stores an image and returns an error if any.
 // This package doesn't have a related interface for simplicity.
 func StoreImage(fileName string, image []byte) error {
 	// STEP 4-4: add an implementation to store an image
+	imgDir := "images"
+	err := os.MkdirAll(imgDir, os.ModePerm)
+	if err != nil {
+		return fmt.Errorf("Error create image directory: %w", err)
+	}
+	
+	hash := sha256.Sum256(image)
+	hashFileName := fmt.Sprint("%x.jpg", hash)
 
+	filepath := filepath.Join(imgDir, hashFileName)
+
+	// Check if file already exists
+	_, err = os.Stat(filepath)
+	if err == nil {
+		return nil 
+	}
+
+	err = os.WriteFile(filepath, image, 0644)
+	if err != nil {
+		return fmt.Errorf("Error writing image to file: %w", err)
+	}
+	
 	return nil
+}
+
+// step 4-5 to findid
+func (i *itemRepository) FindID(ctx context.Context, id int)(*Item, error) {
+	items, err := i.Get(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("Error retriving items: %w", err)
+	}
+	
+	// Check valid ID
+	if id > len(items) || id < 1 {
+		return nil, fmt.Errorf("Item ID does not exist: %w", err)
+	}
+
+	id--
+	return &items[id], nil
 }
