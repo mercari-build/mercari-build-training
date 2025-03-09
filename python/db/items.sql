0514140e7e1bee04d7f32a026bbad99338a9423c
Leoni Angela <87842408+Lyonesse8@users.noreply.github.com>
2025-03-09 15:44:42 +0900

    Update items.sql
    update items.sql to match with step 6 testing
    change category to category_id

--- python/db/items.sql
+++ python/db/items.sql
@@ -1,7 +1,13 @@
+CREATE TABLE IF NOT EXISTS categories (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL COLLATE NOCASE
+);
+
 CREATE TABLE IF NOT EXISTS items (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL COLLATE NOCASE,
-    category TEXT NOT NULL,
-    image_name TEXT NOT NULL
+    category_id TEXT NOT NULL,
+    image_name TEXT NOT NULL,
+    FOREIGN KEY (category_id) REFERENCES categories(id)
 );
 
