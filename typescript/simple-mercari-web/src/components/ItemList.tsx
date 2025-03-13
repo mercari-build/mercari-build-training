e3d03ce49937c8d2651a8327c7b8f87f68f343d6
Leoni Angela <87842408+Lyonesse8@users.noreply.github.com>
2025-03-14 00:13:05 +0900

    Update ItemList.tsx

--- typescript/simple-mercari-web/src/components/ItemList.tsx
+++ typescript/simple-mercari-web/src/components/ItemList.tsx
@@ -6,6 +6,7 @@ const PLACEHOLDER_IMAGE = import.meta.env.VITE_FRONTEND_URL + '/logo192.png';
 interface Prop {
   reload: boolean;
   onLoadCompleted: () => void;
+  keyword: string;
 }
 
 export const ItemList = ({ reload, onLoadCompleted }: Prop) => {
@@ -28,17 +29,27 @@ export const ItemList = ({ reload, onLoadCompleted }: Prop) => {
     }
   }, [reload, onLoadCompleted]);
 
+  const filterItems = (items: Item[], keyword: string) => {
+    return items.filter((item) => item.name.toLowerCase().includes(keyword));
+  };
+
   return (
-    <div>
-      {items?.map((item, index) => {
-      console.debug(`Rendering item:`, item); // Debugging log
-        const imageUrl = item.id ? `http://localhost:9000/image/${item.id}.jpg` : PLACEHOLDER_IMAGE;
-      
+    <div className='ItemListContainer'>
+      {filterItems(items, keyword)?.map((item) => {
+        console.debug(`Rendering item:`, item); // Debugging log
+        // Modified to use the new endpoint that retrieves images by item ID
+        const imageUrl = item.id ? `http://localhost:9000/image/id/${item.id}` : PLACEHOLDER_IMAGE;
+        
         return (
-          <div key={item.id || index} className="ItemList">
-            {/* TODO: Task 2: Show item images */}
-{/*             <img src={PLACEHOLDER_IMAGE} /> */}
-               <img src={imageUrl} alt={item.name || 'Item image'} onError={(e) => (e.currentTarget.src = PLACEHOLDER_IMAGE)} />
+          <div key={item.id} className="ItemList">
+            <img className='item-image'
+            src={imageUrl} 
+            alt={item.name || 'Item image'} 
+            onError={(e) => {
+              console.error(`Image failed to load for item ${item.id}, using placeholder.`);
+              e.currentTarget.src = PLACEHOLDER_IMAGE;
+              }}
+            />
             <p>
               <span>Name: {item.name || 'Unknown'}</span>
               <br />
