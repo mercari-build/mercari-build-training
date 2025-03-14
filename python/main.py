55aa5ad667da684dc3f185f2f4e2c19d771ea09a
Manasa2807 <manasaramavath3@gmail.com>
2025-03-14 07:57:12 +0530

    step-5

--- python/main.py
+++ python/main.py
@@ -1,15 +1,17 @@
 import os
 import logging
 import pathlib
-from fastapi import FastAPI, Form, HTTPException, Depends
+import hashlib
+from fastapi import FastAPI, Form, HTTPException, Depends, File, UploadFile
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 import sqlite3
 from pydantic import BaseModel
 from contextlib import asynccontextmanager
+import json
 
 
-# Define the path to the images & sqlite3 database
+## Define the path to the images & sqlite3 database
 images = pathlib.Path(__file__).parent.resolve() / "images"
 db = pathlib.Path(__file__).parent.resolve() / "db" / "mercari.sqlite3"
 
@@ -18,7 +20,7 @@ def get_db():
     if not db.exists():
         yield
 
-    conn = sqlite3.connect(db)
+    conn = sqlite3.connect(db, check_same_thread=False)
     conn.row_factory = sqlite3.Row  # Return rows as dictionaries
     try:
         yield conn
@@ -28,8 +30,13 @@ def get_db():
 
 # STEP 5-1: set up the database connection
 def setup_database():
-    pass
-
+    conn = sqlite3.connect(db)
+    cursor = conn.cursor()
+    sql_file = pathlib.Path(__file__).parent.resolve() / "db" / "items.sql"
+    with open(sql_file, "r") as f:
+        cursor.executescript(f.read())
+    conn.commit()
+    conn.close()
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -40,7 +47,7 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 logger = logging.getLogger("uvicorn")
-logger.level = logging.INFO
+logger.level = logging.DEBUG
 images = pathlib.Path(__file__).parent.resolve() / "images"
 origins = [os.environ.get("FRONT_URL", "http://localhost:3000")]
 app.add_middleware(
@@ -67,37 +74,109 @@ class AddItemResponse(BaseModel):
 
 # add_item is a handler to add a new item for POST /items .
 @app.post("/items", response_model=AddItemResponse)
-def add_item(
+async def add_item(
     name: str = Form(...),
+    category: str = Form(...),
+    image: UploadFile = File(...),
     db: sqlite3.Connection = Depends(get_db),
 ):
     if not name:
         raise HTTPException(status_code=400, detail="name is required")
 
-    insert_item(Item(name=name))
+    image_name = await hash_and_save_image(image)
+    
+
+
+    cursor = db.cursor()
+    cursor.execute("SELECT id FROM categories WHERE name = ?", (category,))
+    row = cursor.fetchone()
+
+    if not row:
+        cursor.execute("INSERT INTO categories (name) VALUES (?)", (category,))
+        db.commit()
+        category_id = cursor.lastrowid
+    else:
+        category_id = row[0]
+
+    cursor.execute(
+        "INSERT INTO items2 (name, category_id, image_name) VALUES (?, ?, ?)",
+        (name, category_id, image_name)
+    )
+    db.commit()
+
     return AddItemResponse(**{"message": f"item received: {name}"})
 
+class GetItemsResponse(BaseModel):
+    items: list[dict]
+
+@app.get("/items", response_model=GetItemsResponse)
+def get_item(db: sqlite3.Connection = Depends(get_db)):
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM items2")
+    rows = cursor.fetchall()
+    for i in range(len(rows)):
+        rows[i] = dict(rows[i])    
+    return GetItemsResponse(**{"items": rows})
+
+
+@app.get("/items/{item_id}")
+def get_nth_item(item_id: int, db: sqlite3.Connection = Depends(get_db)):
+    if item_id < 1:
+        raise HTTPException(status_code=400, detail="ID should be larger than 1")
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM items2 WHERE id = ?", (item_id,))
+    row = cursor.fetchone()
+    if row is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return row
+
+
+@app.get("/search")
+def search_items(keyword: str, db: sqlite3.Connection = Depends(get_db)):
+    if keyword == "":
+        raise HTTPException(status_code=400, detail="keyword is null")
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM items2 WHERE name LIKE ?", (f"%{keyword}%",))
+    rows = cursor.fetchall()
+    for i in range(len(rows)):
+        rows[i] = dict(rows[i])
+
+    return GetItemsResponse(**{"items": rows})
 
 # get_image is a handler to return an image for GET /images/{filename} .
-@app.get("/images/{image_name}")
+@app.get("/image/{image_name}")
 async def get_image(image_name):
     # Create image path
     image = images / image_name
 
     if not image_name.endswith(".jpg"):
         raise HTTPException(status_code=400, detail="Image path does not end with .jpg")
-
+    
     if not image.exists():
         logger.debug(f"Image not found: {image}")
         image = images / "default.jpg"
 
     return FileResponse(image)
-
-
 class Item(BaseModel):
     name: str
+    category: str
+    image_name: str 
 
+async def hash_and_save_image(image: UploadFile):
+    if not image.filename.endswith(".jpg"):
+        raise HTTPException(status_code=400, detail="Image path does not end with .jpg")
+   
+    sha256 = hashlib.sha256()
 
-def insert_item(item: Item):
-    # STEP 4-2: add an implementation to store an item
-    pass
+    contents = await image.read()
+   
+    sha256.update(contents)
+
+    res = f"{sha256.hexdigest()}.jpg"
+
+    image_path = images / res
+    with open(image_path, "wb") as f:
+        f.write(contents)
+    return res
